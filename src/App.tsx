28957923cc69d71cc07646393297559c12855162
Sylvester Akrong <akrongsylvester@gmail.com>
2025-05-25 16:35:03 +0000

    made some changes to app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import _App from './pages/_app';
 import Footer from './components/Footer';
+import { Analytics } from "@vercel/analytics/react"
 
 const App = () => (
     <div className="min-h-screen flex flex-col">
+        <Analytics />
         <_App />
         <Footer />
     </div>
